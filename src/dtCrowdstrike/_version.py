5da8f4cce7e2db2af357cf5060a4a1eb2dbc8216
Matthew <matthew@digital-thought.org>
2022-12-22 15:20:51 +1100

    Change to Title

--- src/dtCrowdstrike/_version.py
+++ src/dtCrowdstrike/_version.py
@@ -3,6 +3,6 @@ _MAINTAINER = 'Matthew @ Digital-Thought'
 _AUTHOR = 'Digital-Thought'
 _AUTHOR_EMAIL = 'matthew@digital-thought.org'
 _DESCRIPTION = "The CrowdStrike Falcon RESTapi Helper Library"
-_TITLE = "digital-thought-crowdstrike"
+_TITLE = "dtCrowdstrike"
 _PROJECT_URL = ""
 _DOCS_URL = ""
